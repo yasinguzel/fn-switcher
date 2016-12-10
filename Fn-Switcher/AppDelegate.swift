f098ee30493e85c35e8cd88a22551e8bc2be6e35
guzelyasin <guzelyasin@icloud.com>
2016-12-10 09:34:17 +0300

    Initial Commit

---
+++ Fn-Switcher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Fn-Switcher
+//
+//  Created by Yasin GÜZEL on 10.12.2016.
+//  Copyright © 2016 Yasin GÜZEL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
