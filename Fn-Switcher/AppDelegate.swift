06a730604e02b24c84465127d7b64c0b924f7913
guzelyasin <guzelyasin@icloud.com>
2016-12-11 23:04:54 +0300

    Edited icon for dark theme.

--- Fn-Switcher/AppDelegate.swift
+++ Fn-Switcher/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var statusMenu: NSMenu!
     let userName = NSUserName()
-
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
     
     @IBAction func quitClicked(_ sender: NSMenuItem) {
@@ -22,16 +21,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @IBAction func switchClicked(_ sender: Any) {
-
         let task = Process()
         task.launchPath = "/usr/bin/osascript"
-        task.arguments = ["/Users/"+userName+"/Documents/Fn-Switcher/Fn-Switcher.scpt"]//this line will edited.
+        task.arguments = ["/Users/"+userName+"/Documents/Fn-Switcher/AppleScript/Fn-Switcher.scpt"]//this line will edited.
         task.launch()
     }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         let icon = NSImage(named: "statusIcon")
+        icon?.isTemplate = true
         statusItem.image = icon
         statusItem.menu = statusMenu
     }
@@ -39,7 +38,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
 
 }
 
