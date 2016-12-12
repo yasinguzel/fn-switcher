c946fc834f2087b19fa2c949f7e59042ad70c82b
guzelyasin <guzelyasin@icloud.com>
2016-12-12 23:54:52 +0300

    Solved file path problem.

--- Fn-Switcher/AppDelegate.swift
+++ Fn-Switcher/AppDelegate.swift
@@ -13,10 +13,9 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var statusMenu: NSMenu!
-    let userName = NSUserName()
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
-    
-    
+
+
     @IBAction func quitClicked(_ sender: NSMenuItem) {
         NSApplication.shared().terminate(self)
     }
@@ -24,24 +23,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBAction func switchClicked(_ sender: Any) {
         let task = Process()
         task.launchPath = "/usr/bin/osascript"
-        task.arguments = ["/Users/"+userName+"/Documents/Fn-Switcher/AppleScript/Fn-Switcher.scpt"]//this line will edited.
+        let filePath = Bundle.main.path(forResource:"Fn-Switcher", ofType: "scpt")
+        task.arguments = [filePath!]//this line will edited.
         task.launch()
     }
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         let icon = NSImage(named: "statusIcon")
         icon?.isTemplate = true
         statusItem.image = icon
         statusItem.menu = statusMenu
-
-        
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
     
-
 }
 
